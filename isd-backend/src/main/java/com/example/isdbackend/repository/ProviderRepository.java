eb6b43a6c1ebbf5b991ec973a5dd798b31bd3981
stelajg <stela.gulica@gmail.com>
2020-08-06 17:23:57 +0300

    provider repo

---
+++ isd-backend/src/main/java/com/example/isdbackend/repository/ProviderRepository.java
@@ -0,0 +1,10 @@
+package com.example.isdbackend.repository;
+
+import com.example.isdbackend.model.Provider;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProviderRepository extends CrudRepository<Provider, Integer> {
+
+}
