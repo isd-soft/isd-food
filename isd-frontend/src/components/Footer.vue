7c462350ed24cd9cebe07d7b3929d3a9e4008869
Alexei Murinets <Alexei7a@gmail.com>
2020-08-08 13:33:03 +0300

    header and footer

---
+++ isd-frontend/src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-footer
+    absolute
+    color="warning"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="orange py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Isd-Food</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Home',
+        'Menus',
+        'About Us',
+        'Contact Us',
+      ],
+    }),
+  }
+</script>
