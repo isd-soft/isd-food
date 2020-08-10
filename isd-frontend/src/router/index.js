e98c5b9b6a9e5151bbeb4156bca58ea41ff47aa4
Nichita Dolinschi <65854353+NichitaDolinschi@users.noreply.github.com>
2020-08-10 10:50:24 +0300

    Clean

--- isd-frontend/src/router/index.js
+++ isd-frontend/src/router/index.js
@@ -32,6 +32,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/user/{id}/edit",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/UserEdit.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ isd-frontend/src/views/UserEdit.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <v-container>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+name: "UserEdit"
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
