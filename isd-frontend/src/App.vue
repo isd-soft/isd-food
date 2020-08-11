0dafbaec992290817eca67bdb86dfa56d38f228d
Nichita Dolinschi <65854353+NichitaDolinschi@users.noreply.github.com>
2020-08-11 16:39:56 +0300

    Clean

--- isd-frontend/src/App.vue
+++ isd-frontend/src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <v-app>
-    <v-main>
-      <Header/>
-      <router-view/>
-      <Footer/>
-    </v-main>
+
   </v-app>
 </template>
 
 <script>
 
-import Footer from './components/Footer';
-import Header from './components/Header';
 
 export default {
   name: 'App',
 
-  components: {
-    Footer,
-    Header,
-  },
+
 
   data: () => ({
     //
