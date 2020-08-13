a855078e6a45a05f7b4f7b95800166e9296941dc
Nichita Dolinschi <65854353+NichitaDolinschi@users.noreply.github.com>
2020-08-13 17:26:30 +0300

    Update Login.vue

--- isd-frontend/src/views/Login.vue
+++ isd-frontend/src/views/Login.vue
@@ -65,12 +65,7 @@
                               </v-col>
                             </v-form>
 
-                            <div class="form-group">
-                              <div class="custom-control custom-checkbox small">
-                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                <label class="custom-control-label" for="customCheck">Remember Me</label>
-                              </div>
-                            </div>
+                           
 
 
 
