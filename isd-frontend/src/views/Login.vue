a021dd97dfcb134ff4b3a125ba1610349daaef04
Nichita Dolinschi <65854353+NichitaDolinschi@users.noreply.github.com>
2020-08-13 17:13:57 +0300

    Update Login.vue

--- isd-frontend/src/views/Login.vue
+++ isd-frontend/src/views/Login.vue
@@ -1,46 +1,77 @@
 <template>
-  <v-container>
-    <Header/>
-    <v-row justify="center">
-      <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
-        @submit.prevent="submitHandler"
-        id="login-form"
-      >
-        <v-col sm="12" md="12">
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="Email"
-            outlined
-            required
-          ></v-text-field>
+  <v-app>
+    <v-overlay
+        :absolute="absolute"
+        :opacity="opacity"
+        :value="overlay"
+        :z-index="zIndex"
+    >
 
-          <v-text-field
-            type="password"
-            v-model="password"
-            :rules="passwordRules"
-            label="Password"
-            outlined
-            required
-          ></v-text-field>
+      <v-app>
+        <v-card width="100vw" height="100vh" class="mx-auto">
+          <body class="bg-gradient-warning">
 
-          <v-btn
-            class="col-12"
-            color="warning"
-            depressed="true"
-            type="submit"
-            form="login-form"
-            :disabled="$store.getters.isLoggingInProcess"
-            :loading="$store.getters.isLoggingInProcess"
-            >Login</v-btn
-          >
-        </v-col>
-      </v-form>
-    </v-row>
-  </v-container>
+          <div class="container " style="padding-top: 20vh" >
+
+            <!-- Outer Row -->
+            <div class="row justify-content-center ">
+
+              <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                  <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                      <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                      <div class="col-lg-6">
+                        <div class="p-5">
+                          <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">Welcome to isd-food</h1>
+                          </div>
+                          <v-form class="user">
+
+
+                            <div class="form-group">
+                              <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                            </div>
+                            <div class="form-group">
+                              <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                              <div class="custom-control custom-checkbox small">
+                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                <label class="custom-control-label" for="customCheck">Remember Me</label>
+                              </div>
+                            </div>
+                            <a href="index.html" class="btn btn-warning btn-user btn-block" style="color: white">
+                              Login
+                            </a>
+                          </v-form>
+                          <hr>
+                          <div class="text-center">
+                            <a class="small" href="forgot-password.html">Forgot Password?</a>
+                          </div>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+
+
+          </body>
+        </v-card>
+      </v-app>
+    </v-overlay>
+  </v-app>
+
 </template>
 <script>
 
