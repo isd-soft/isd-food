2bd0b13c2f9524fe7ae60cfab54c3876dfc390d5
Nichita Dolinschi <65854353+NichitaDolinschi@users.noreply.github.com>
2020-08-14 14:49:18 +0300

    Update Menu

--- isd-frontend/src/components/MenuItem.vue
+++ isd-frontend/src/components/MenuItem.vue
@@ -1,99 +1,179 @@
 <template>
+  <div class=" col-md-4 col-sm-6 mb-4">
+    <div class="card border-left-warning shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-warning ml-1 text-uppercase mb-1" >{{product_data.name}}</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 12px">
+              <div class="ml-1">
+                <p>Provider : {{product_data.provider.name}}</p>
+                <div>
+                  <img src="https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg" class="image" alt="">
+                </div>
+                <v-app style="height: auto; max-height: 230px">
+                  <v-radio-group class = "ml-1" v-model="type" id="type" :mandatory="false" style="color: orange !important" row>
+                    <v-radio
+                        label="M"
+                        color="warning"
+                        value="M">
+                    </v-radio>
+                    <v-radio
+                        label="S"
+                        color="warning"
+                        value="S"
+                    ></v-radio>
+                  </v-radio-group>
+                  <div v-if= "type === 'M'">
+                    <div v-for="item in product_data.menuTypes" :key="item.type">
+                      <div v-if= "item.type === 'M'">
+                      </div>
+                    </div>
+                    {{check_menu('M')}}
+                  </div>
+                  <div v-else>
+                    {{check_menu('S')}}
+                  </div>
 
-    <div class="item my-3"  >
-    <div>
-     <v-img  class="image" src="https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg" aspect-ratio="1.7"></v-img>
-    </div>
-         <br>
-         <p>Name: {{product_data.name}}</p>
-         <p>Provider: {{product_data.provider.name}}</p>
+                  <p>Items:</p>
 
-            <v-radio-group class="radio-group" v-model="type" id="type" :mandatory="false" row >
-            <span class="pr-3" style="color:black">Type:</span>
-            <div class="row" style="color:black">
-              <v-radio
-                  label="M"
-                  color="warning" 
-                  value="M">
-              </v-radio>
-              <v-radio
-                label="S"
-                color="warning"
-                value="S"
-              ></v-radio>
+                  <li v-for="item in product_data.menuTypes[(type_id)].items" :key="item.name">
+                    {{ item.name }}
+                  </li>
+                  <br>
+                  <!-- <p>Delivery price: {{product_data.provider.deliveryPrice}} mdl.</p>
+                    <p>Price: {{product_data.menuTypes[(type_id)].price}} mdl.</p>
+                    <p>Total price: {{Number(product_data.provider.deliveryPrice) + Number(product_data.menuTypes[(type_id)].price)}}  mdl.</p>
+                   <br>-->
+
+
+                  <div class="text-center">
+                    <v-dialog
+                        v-model="dialog"
+                        width="500"
+                    >
+
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-card-actions class="justify-center" >
+                          <v-btn style="border-radius: 5px !important;"
+                                 @click="makeOrder()"
+                                 v-bind="attrs"
+                                 v-on="on"
+                                 large
+                                 rounded
+                                 color="warning"
+                                 align="center">
+                            Order
+                          </v-btn>
+                        </v-card-actions>
+
+                      </template>
+                      <v-card>
+                        <v-card-title class="headline grey lighten-2">
+                          Order confirmed!
+                        </v-card-title>
+
+                        <v-card-text>
+                          Enjoy your food! <br>
+                          Isd-food with love🧡
+                        </v-card-text>
+
+                        <v-divider></v-divider>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn
+                              color="warning"
+                              text
+                              @click="dialog = false"
+                          >
+                            Excelent
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                  </div>
+                </v-app>
+              </div>
             </div>
-          </v-radio-group>
-       
-         <div v-if= "type === 'M'">
-             <div v-for="item in product_data.menuTypes" :key="item.type">
-                 <div v-if= "item.type === 'M'">
-                 </div>
-             </div>
-             {{check_menu('M')}}
-         </div>
-         <div v-else>
-            {{check_menu('S')}}
+          </div>
         </div>
+      </div>
+    </div>
 
-         <p>Items:</p>
-
-            <li v-for="item in product_data.menuTypes[(type_id)].items" :key="item.name">
-            {{ item.name }}
-            </li>
-    <br>
-        <!-- <p>Delivery price: {{product_data.provider.deliveryPrice}} mdl.</p>
-          <p>Price: {{product_data.menuTypes[(type_id)].price}} mdl.</p>
-          <p>Total price: {{Number(product_data.provider.deliveryPrice) + Number(product_data.menuTypes[(type_id)].price)}}  mdl.</p>
-         <br>-->
-
-
-        <div class="text-center">
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-
-      <template v-slot:activator="{ on, attrs }">
-          <v-card-actions class="justify-center" >
-         <v-btn 
-         @click="makeOrder()" 
-          v-bind="attrs"
-          v-on="on"
-         large 
-         rounded
-          color="warning" 
-          align="center">
-            Order
-          </v-btn>
-        </v-card-actions>   
-
-      </template>
-      <v-card>
-        <v-card-title class="headline grey lighten-2">
-          Order confirmed!
-        </v-card-title>
-
-        <v-card-text>
-          Enjoy your food! <br>
-          Isd-food with love🧡
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="warning"
-            text
-            @click="dialog = false"
-          >
-            Excelent
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </div>
- </div>
+
+
+
+<!--  <div v-if= "type === 'M'">-->
+<!--      <div v-for="item in product_data.menuTypes" :key="item.type">-->
+<!--        <div v-if= "item.type === 'M'">-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      {{check_menu('M')}}-->
+<!--    </div>-->
+<!--    <div v-else>-->
+<!--      {{check_menu('S')}}-->
+<!--    </div>-->
+
+<!--    <p>Items:</p>-->
+
+<!--    <li v-for="item in product_data.menuTypes[(type_id)].items" :key="item.name">-->
+<!--      {{ item.name }}-->
+<!--    </li>-->
+<!--    <br>-->
+<!--    &lt;!&ndash; <p>Delivery price: {{product_data.provider.deliveryPrice}} mdl.</p>-->
+<!--      <p>Price: {{product_data.menuTypes[(type_id)].price}} mdl.</p>-->
+<!--      <p>Total price: {{Number(product_data.provider.deliveryPrice) + Number(product_data.menuTypes[(type_id)].price)}}  mdl.</p>-->
+<!--     <br>&ndash;&gt;-->
+
+
+<!--    <div class="text-center">-->
+<!--      <v-dialog-->
+<!--          v-model="dialog"-->
+<!--          width="500"-->
+<!--      >-->
+
+<!--        <template v-slot:activator="{ on, attrs }">-->
+<!--          <v-card-actions class="justify-center" >-->
+<!--            <v-btn-->
+<!--                @click="makeOrder()"-->
+<!--                v-bind="attrs"-->
+<!--                v-on="on"-->
+<!--                large-->
+<!--                rounded-->
+<!--                color="warning"-->
+<!--                align="center">-->
+<!--              Order-->
+<!--            </v-btn>-->
+<!--          </v-card-actions>-->
+
+<!--        </template>-->
+<!--        <v-card>-->
+<!--          <v-card-title class="headline grey lighten-2">-->
+<!--            Order confirmed!-->
+<!--          </v-card-title>-->
+
+<!--          <v-card-text>-->
+<!--            Enjoy your food! <br>-->
+<!--            Isd-food with love🧡-->
+<!--          </v-card-text>-->
+
+<!--          <v-divider></v-divider>-->
+
+<!--          <v-card-actions>-->
+<!--            <v-spacer></v-spacer>-->
+<!--            <v-btn-->
+<!--                color="warning"-->
+<!--                text-->
+<!--                @click="dialog = false"-->
+<!--            >-->
+<!--              Excelent-->
+<!--            </v-btn>-->
+<!--          </v-card-actions>-->
+<!--        </v-card>-->
+<!--      </v-dialog>-->
+<!--    </div>-->
 </template>
 
 <script>
@@ -149,7 +229,9 @@ props:{
     height: 500px;
 }
 .image{
-    border-radius: 15px;
-    box-shadow: 0 0 8px 0 orange;
+  background-size: cover;
+  width: 100%;
+  border-radius: 4px;
+
 }
 </style>
--- isd-frontend/src/components/MenuList.vue
+++ isd-frontend/src/components/MenuList.vue
@@ -1,242 +1,12 @@
 <template>
-<div class="menu">
-  <div class="text-center">
-    <h1>Menus</h1>
-    <v-btn @click="callMenuApi()" color="warning">Show menu</v-btn>
-  </div>
-<br>
-<br>
- <!-- <v-carousel
-    cycle
-    height="400"
-    hide-delimiter-background
-    show-arrows-on-hover
-  >
-    <v-carousel-item
-     v-for="product in products"
-     :key="product.name"
-    >
+  <div class="row">
+    {{callOnce()}}
     <MenuItem
-     :product_data="product"
-     />
-
-    </v-carousel-item>
-  </v-carousel> -->
-  <!-- <v-carousel
-    cycle
-    height="400"
-    hide-delimiter-background
-    show-arrows-on-hover
-  >
-    <v-carousel-item
-     v-for="product in products"
-     :key="product.name"
-    >
-          <div class="display-3">
-            <MenuItem
-              :product_data="product"
-            />
-          </div>
-
-    </v-carousel-item>
-  </v-carousel> -->
-
-  <!-- <v-carousel hide-delimiters height="750" show-arrows-on-hover cycle>
-    <v-carousel-item
-     v-for="product in products"
-     :key="product.name"
-    >
-      <v-row class="fill-height" align="center" justify="center">
-        <div class="display-3 white--text"><MenuItem
-              :product_data="product"
-            /></div>
-      </v-row>
-    </v-carousel-item>
-  </v-carousel> -->
-
-  <!-- <v-carousel>
-    <v-carousel-item
-      v-for="product in products"
-      :key="product.name"
-      :product_data="product"
-      reverse-transition="fade-transition"
-      transition="fade-transition"
-    ></v-carousel-item>
-  </v-carousel> -->
-
-<!-- <div id="header-carousel">
-      <v-carousel style="height: 100%; width: 100%">
-        <v-carousel-item style="height: 100%; width: 100%"
-     v-for="product in products"
-     :key="product.name">
-          <v-jumbotron dark>
-            <v-container fill-height fill-width>
-              <v-layout align-center>
-                <v-flex>
-                  <span class="subheading"> <MenuItem
-              :product_data="product"
-            /></span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-jumbotron>
-        </v-carousel-item>
-      </v-carousel>
-    </div> -->
-
-     <!-- <div class="menu_list">
-     <MenuItem
-     v-for="product in products"
-     :key="product.name"
-     :product_data="product"
-     />
-     </div> -->
-     
-    <!-- <v-row>
-    <v-col cols="12" sm="3" md="4" v-for="product in products"
-     :key="product.name" >
-        <MenuItem :product_data="product" />
-    </v-col>
-    </v-row> -->
-    <v-container fluid grid-list-xl>
-    <v-row no-gutters
-    :align="alignment"
-          :justify="justify-content">
-      <v-col
         v-for="product in products"
         :key="product.name"
-        cols="12"
-        sm="4"
-      >
-        <!-- <v-card shaped
-          class="pa-4 mx-lg-auto ma-3 mx-5 rounded-card"
-          color="white"
-          width="250px"
-        > -->
-          <MenuItem :product_data="product" />
-        <!-- </v-card> -->
-      </v-col>
-    </v-row>
-  </v-container>
-  <!-- <v-container
-      v-for="align in alignments"
-      :key="align"
-    >
-      <v-row
-        :align="align"
-        no-gutters
-        style="height: 150px;"
-      >
-        <v-col
-          v-for="product in products"
-          :key="product.name"
-        >
-          <v-card
-            class="pa-2"
-            outlined
-            tile
-          >
-            <MenuItem :product_data="product" />
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container> -->
+        :product_data="product"/>
 
-
-
-
-<!--<div class="container-fluid">
-<div class="row">
-  <div class="col-md-3 col-lg-5 mb-3" style="background: red; border-radius:40px">
-    as
   </div>
-  <div class="col-auto"></div>
-    <div class="col-md-3 col-lg-5 mb-3" style="background: red; border-radius:40px">
-    as
-  </div>
-  <div class="col-auto"></div>
-    <div class="col-md-3 col-lg-5 mb-3" style="background: red; border-radius:40px">
-    as
-  </div>
-  <div class="col-auto"></div>
-    <div class="col-md-3  col-lg-5 mb-3" style="background: red; border-radius:40px">
-    as
-  </div>
-  <div class="col-auto"></div>
-    <div class="col-md-3 col-lg-5 mb-3" style="background: red; border-radius:40px">
-    as
-  </div>
-  <div class="col-auto"></div>
-  
-  
-</div>    
-</div>-->
-
-<!-- <v-card
-    color="black"
-    dark
-    flat
-    tile
-  >
-    <v-window v-model="onboarding">
-      <v-window-item
-        v-for="n in length"
-        :key="`card-${n}`"
-      >
-        <v-card
-          color="transparent"
-          height="200"
-        >
-          <v-row
-            class="fill-height"
-            align="center"
-            justify="center"
-            tag="v-card-text"
-          >
-            <MenuItem :product_data="product"
-/>
-          </v-row>
-        </v-card>
-      </v-window-item>
-    </v-window>
-
-    <v-card-actions class="justify-space-between">
-      <v-btn
-        text
-        @click="prev"
-      >
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
-      <v-item-group
-        v-model="onboarding"
-        class="text-center"
-        mandatory
-      >
-        <v-item
-          v-for="n in length"
-          :key="`btn-${n}`"
-          v-slot:default="{ active, toggle }"
-        >
-          <v-btn
-            :input-value="active"
-            icon
-            @click="toggle"
-          >
-            <v-icon>mdi-record</v-icon>
-          </v-btn>
-        </v-item>
-      </v-item-group>
-      <v-btn
-        text
-        @click="next"
-      >
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>-->
-</div> 
-
-
 </template>
 
 <script>
@@ -254,6 +24,7 @@ import MenuItem from './MenuItem.vue'
         menu_types: [],
         helloResponse: [],
         errors: [],
+        get: false,
         alignments: [
         'start',
         'center',
@@ -263,6 +34,12 @@ import MenuItem from './MenuItem.vue'
     },
     methods: {
       // Fetches posts when the component is created.
+      callOnce(){
+        if(!this.get){
+          this.callMenuApi();
+          this.get = true
+        }
+      },
       callMenuApi () {
         api.getMenu().then(response => {
             this.products = response.data;
